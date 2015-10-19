70b2352a5d19613622782f269b15f733abd3013f
Dario Oliveri <github.com/Darelbi>
2015-10-19 15:59:51 +0200

    Fixing assertion macro

--- badeSource/Assert.hpp
+++ badeSource/Assert.hpp
@@ -9,4 +9,4 @@ namespace Bade{
 	void BADE_API failure( const char* code, const char* file, const int line);
 }
 
-#define assert(x)do{if(!(x))Bade::failure(#x,__FILE__,__LINE__);}while(false)
+#define assert(x){do{if(!(x))Bade::failure(#x,__FILE__,__LINE__);}while(false);}
