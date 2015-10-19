82b2fe7020f4aa145df939511aa6ffae05365a42
Dario Oliveri <github.com/Darelbi>
2015-10-19 16:14:41 +0200

    added dummy return value
    visual studio is more strict than GCC/Clang and I don't want to bother
    find wich option touch in one of those compilers.

--- badeSource/gl3/GL3TextureManager.cpp
+++ badeSource/gl3/GL3TextureManager.cpp
@@ -8,50 +8,50 @@
 
 namespace Bade {
 namespace GL3 {
-	
+
 GL3TextureManager::GL3TextureManager(){
-	
+
 }
-	
+
 TexturePtr GL3TextureManager::getTexture( 	BitmapImagePtr & image,
 											bool mipmaps){
-									
+	return nullptr;
 }
-								
+
 void GL3TextureManager::reloadTexture( 	BitmapImagePtr image,
 										TexturePtr & texture){
-									
+
 }
 
 TexturePtr GL3TextureManager::shallowCopy( TexturePtr & texture){
-									
+	return nullptr;
 }
 
 SamplerPtr GL3TextureManager::getSampler( 	FilteringMode 		filtering,
 											TextureWrap			wrap,
 											float 				anisotropy) const{
-									
+	return nullptr;
 }
-								
+
 TextureSlotPtr GL3TextureManager::getTextureSlot( 	u8 textureUnit,
 													TexturePtr & texture,
 													SamplerPtr & sampler){
-									
+	return nullptr;
 }
 
 void GL3TextureManager::setTextureSlot( RenderSlot & slot,
 										TextureSlotPtr & textureSlot){
-									
+
 }
-							 
+
 TextureSlotPtr GL3TextureManager::shallowCopy( TextureSlotPtr & textureSlot){
-									
+	return nullptr;
 }
 
 void GL3TextureManager::freeUnusedTextures(){
 	textures.collectSome( textures.size(), []( GL3Texture&){} );
-	textures.shrinkToFit();								
+	textures.shrinkToFit();
 }
-	
+
 } // namespace GL3
 } // namespace Bade
